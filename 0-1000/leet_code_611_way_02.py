96017c0a12d36ac27ce941d10061e16c9252b597
ThinhBanh <thinhbanhphuc@gmail.com>
2025-09-26 22:22:27 +0700

    Today push

---
+++ 0-1000/leet_code_611_way_02.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def triangleNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n < 3:
+            return 0
+        nums.sort()
+        count = 0
+        for i in range(2, n):
+            left = 0
+            right = i-1
+            while left < right:
+                if nums[left] + nums[right] > nums[i]:
+                    count += right - left
+                    right -= 1
+                else:
+                    left += 1
+
+        return count
+
+print(Solution().triangleNumber([2, 2, 3, 4]))  # Output: 3
+print(Solution().triangleNumber([4,2,3,4]))  # Output: 4

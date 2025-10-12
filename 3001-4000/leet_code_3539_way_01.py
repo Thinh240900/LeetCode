81f3bead208196b1aef1e2cb28c96c2140d6d413
ThinhBanh <thinhbanhphuc@gmail.com>
2025-10-12 13:56:33 +0700

    Today push

---
+++ 3001-4000/leet_code_3539_way_01.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def magicalSum(self, m: int, k: int, nums: List[int]) -> int:
+        # Decide drop
+        return 0
+
+print(Solution().magicalSum(m = 5, k = 5, nums = [1,10,100,10000,1000000])) # 991600007
+print(Solution().magicalSum( m = 2, k = 2, nums = [5,4,3,2,1])) # 170
+print(Solution().magicalSum( m = 1, k = 1, nums = [28])) # 28
